d267fce69366ac96b79e953410988a5499dd5971
jvdionizio <dionizio145@gmail.com>
2021-12-31 11:42:15 -0300

    adicionando requisitos até o 7

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// eslint-disable-next-line max-lines-per-function
 window.onload = function pgCarregada() {
   const colorBoard = document.getElementById('color-board');
   const random = Math.round(Math.random() * 6);
   const rgbColor = document.getElementById('rgb-color');
   const answer = document.getElementById('answer');
   const resetGame = document.getElementById('reset-game');
+  const score = document.getElementById('score');
+  let scoreNumber = 0;
+
+  score.innerText = scoreNumber;
 
   function generateColor() {
-    let color = [];
+    const color = [];
     for (let i = 0; i < 3; i += 1) {
       color.push(Math.round(Math.random() * 256));
     }
@@ -16,11 +21,12 @@ window.onload = function pgCarregada() {
   function createDiv(className) {
     const div = document.createElement('div');
     div.className = className;
-    div.addEventListener('click',answering);
+    // eslint-disable-next-line no-use-before-define
+    div.addEventListener('click', answering);
     return div;
   }
 
-  function createColors(){
+  function createColors() {
     for (let i = 0; i < 6; i += 1) {
       colorBoard.appendChild(createDiv('ball'));
     }
@@ -28,14 +34,14 @@ window.onload = function pgCarregada() {
 
   createColors();
 
-  function aplingColor(){
+  function aplingColor() {
     for (let i = colorBoard.childNodes.length - 1; i >= 0; i -= 1) {
       colorBoard.childNodes[i].style.backgroundColor = generateColor();
     }
   }
   aplingColor();
 
-  function selectingColor(){
+  function selectingColor() {
     colorBoard.childNodes[random].classList.add('selected');
     const selected = document.querySelector('.selected');
     const selectedCs = window.getComputedStyle(selected);
@@ -49,11 +55,13 @@ window.onload = function pgCarregada() {
 
   function answering(event) {
     if (event.target.classList.contains('selected')) {
-      answer.innerText = 'Acertou!'
+      answer.innerText = 'Acertou!';
+      scoreNumber += 3;
+      score.innerText = scoreNumber;
     } else {
-      answer.innerText = 'Errou! Tente novamente!'
+      answer.innerText = 'Errou! Tente novamente!';
     }
-  };
+  }
 
   function reset() {
     for (let i = colorBoard.childNodes.length - 1; i >= 0; i -= 1) {
